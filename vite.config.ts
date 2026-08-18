0a66d2727d02dbef7ea4b293a94c1600be0647f3
jotafiiles <cljotacodeyt@gmail.com>
2026-08-18 19:32:18 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Panel-Cdlc/',
+    base: '/PANEL-CDC/',
 
     plugins: [react(), tailwindcss()],
 
